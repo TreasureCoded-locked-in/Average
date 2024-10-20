9bb35d3a0b27aa0f58712190493d86aac1d29b4c
TreasureCoded-locked-in <akpannsikak410@gmail.com>
2024-10-20 16:13:02 +0100

    First Commit

---
+++ main.py
@@ -0,0 +1,14 @@
+# Storing values
+tree1 = 90
+tree2 = 94
+tree3 = 45
+tree4 = 30
+tree5 = 18
+
+# Finding the total of trees
+sum = tree1+tree2+tree3+tree4+tree5
+print('the sum of all the 5 trees is: ', sum)
+
+#FInding the average of trees
+average = sum/5
+print('the average of all the tree is :', average)
